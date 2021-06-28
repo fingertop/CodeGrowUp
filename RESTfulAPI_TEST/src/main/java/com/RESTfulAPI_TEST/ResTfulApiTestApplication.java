2972ac07b8bb2158ad13ec2749d09b611b7fc09d
jianhua.li <fingertop@foxmail.com>
2021-06-28 20:09:14 +0800

    RESTfulAPI_TEST

---
+++ RESTfulAPI_TEST/src/main/java/com/RESTfulAPI_TEST/ResTfulApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.restfulapi_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulApiTestApplication.class, args);
+    }
+
+}
