495c2a5b7ac90a8c777dbb32cdba56a189c3e805
gfingertop <fingertop@foxmail.com>
2021-07-06 19:39:31 +0800

    Flyway管理数据库版本

---
+++ FlywayUseDatabasesVersion/src/main/java/com/flywayusedatabasesversion/FlywayUseDatabasesVersionApplication.java
@@ -0,0 +1,13 @@
+package com.flywayusedatabasesversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayUseDatabasesVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayUseDatabasesVersionApplication.class, args);
+    }
+
+}
