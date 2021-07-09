3103c988708459b86497aecd38a8a304d00d6927
gfingertop <fingertop@foxmail.com>
2021-07-09 17:34:57 +0800

    Thymeleaf Web

---
+++ ThymeleafWeb/src/test/java/com/example/thymeleafweb/ThymeleafWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.thymeleafweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
