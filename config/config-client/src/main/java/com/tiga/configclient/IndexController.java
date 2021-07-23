1e9e0f9e33cefc31e4bf725e7069eedc26e2bf89
lijianhua <fingertop@foxmail.com>
2021-07-23 17:11:37 +0800

    分布式配置中心【Dalston版】 （高可用与动态刷新）

--- config/config-client/src/main/java/com/tiga/configclient/IndexController.java
+++ config/config-client/src/main/java/com/tiga/configclient/IndexController.java
@@ -1,9 +1,11 @@
 package com.tiga.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/index")
 public class IndexController {
