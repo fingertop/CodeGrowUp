233eb19d85a4ef3358d9112adb3187fa5411dd9a
lijianhua <fingertop@foxmail.com>
2021-07-26 16:29:22 +0800

    Hystrix监控面板【Dalston版】

--- Hystrix/eureka-consumer-ribbon-hystrix/README.md
+++ Hystrix/eureka-consumer-ribbon-hystrix/README.md
@@ -2,4 +2,5 @@
 
 # 延伸阅读
 *   https://blog.didispace.com/spring-cloud-starter-dalston-4-1/
+*   https://blog.didispace.com/spring-cloud-starter-dalston-5-1/
 
--- Hystrix/eureka-consumer-ribbon-hystrix/src/main/java/com/tiga/eurekaconsumerribbonhystrix/EurekaConsumerRibbonHystrixApplication.java
+++ Hystrix/eureka-consumer-ribbon-hystrix/src/main/java/com/tiga/eurekaconsumerribbonhystrix/EurekaConsumerRibbonHystrixApplication.java
@@ -1,14 +1,18 @@
 package com.tiga.eurekaconsumerribbonhystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrixDashboard
 @SpringCloudApplication
 // @EnableCircuitBreaker
 // @EnableDiscoveryClient
@@ -23,6 +27,20 @@ public class EurekaConsumerRibbonHystrixApplication {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * Unable to connect to Command Metric Stream 由于spring cloud版本问题使用手动Bean
+     *
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
