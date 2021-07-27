eaac4cee4f08faa96144e07e1dad0df36f40a8b4
lijianhua <fingertop@foxmail.com>
2021-07-27 11:38:14 +0800

    Spring Cloud构建微服务架构：服务网关（过滤器）【Dalston版】

--- gateway/api-gateway/src/main/java/com/tiga/apigateway/ApiGatewayApplication.java
+++ gateway/api-gateway/src/main/java/com/tiga/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,21 @@
 package com.tiga.apigateway;
 
+import com.tiga.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class ApiGatewayApplication {
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
